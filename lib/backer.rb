2609793581bc73ecb108305a4b498e03e3c21b9e
Joseph DeGregorio <degregorio.joseph@gmail.com>
2020-03-01 01:33:37 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,7 +1,5 @@
-require 'pry'
-
 class Backer
-attr_accessor
+
 attr_reader :backed_projects, :name
 
 def initialize (name)
@@ -13,6 +11,4 @@ def back_project (project)
   @backed_projects << project
   project.add_backer(self) unless project.backers.include?(self)
 end
-
-
 end
